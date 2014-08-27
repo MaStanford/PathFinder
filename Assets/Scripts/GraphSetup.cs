23836a636e9dd54a57830f2e0756a60c02e0e1d9
voznesenskym@gmail.com <voznesenskym@gmail.com>
2014-08-27 11:52:51 -0700

    file structure + stuff

---
+++ Assets/Scripts/GraphSetup.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GraphSetup : Object {
+	#region Fields
+	List<List<T>> ListOfLists;
+	#endregion
+	
+	#region Methods
+	// Use this for initialization
+	void Start () {
+//		ListOfLists = List
+		ListOfLists = new List<List<T>>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	#endregion
+}
---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Node : MonoBehaviour {
+	#region Fields
+	#endregion
+
+	#region Methods
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	#endregion
+}
